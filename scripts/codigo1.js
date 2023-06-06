ebb8fdae010b8536ed661652fa5fc564ea30806c
santiifernandezz <santythomasfp@gmail.com>
2023-06-06 00:31:00 -0300

    ultimo

--- scripts/codigo1.js
+++ scripts/codigo1.js
@@ -1,3 +1,17 @@
-function Enviado(){
-    alert("Tu formulario se envio con exito, Gracias por contactarnos");
+
+function validarfor(){
+
+var email = document.getElementById("email").value; 
+var nombres = document.getElementsByName("nombres")[0].value;
+var doc = document.getElementsByName("documento")[0].value;
+var tel = document.getElementsByName("telefono")[0].value;
+var acepta = document.getElementsByName("acepta")[0].value;
+var sedes = document.getElementsByName("sedes")[0].value;
+
+
+if ( validaVacio(correo.value) || validaVacio(nombres.value) || validaVacio(doc.value) || validaVacio(tel.value) || validaVacio(acepta.value) || validaVacio(sedes.value)) { 
+    alert("Los campos no pueden quedar vacios");
+    return false;
+}
+    return true;
 }
